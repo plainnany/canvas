76a57ceb0e61eebd692a9b4bc278ec6f1130360f
plainnany <plainnany@gmail.com>
2017-07-17 12:05:16 +0800

    使用rem解决

--- script.js
+++ script.js
@@ -6,42 +6,55 @@ var context=canvas.getContext('2d')
 var previousPiont
 
 canvas.width=document.documentElement.clientWidth
-canvas.height=document.documentElement.clientHeight-40
+canvas.height=document.documentElement.clientHeight-158
 
 canvas.addEventListener('touchmove',function(e){
-  e.preventDefault()
+    e.preventDefault()
 
-  var pen = document.querySelector('input[name="penType"]:checked').value
+    var pen = document.querySelector('input[name="penType"]:checked').value
      
-  var pageX=e.touches[0].pageX
-  var pageY=e.touches[0].pageY
-  if(pen==='pen'){
-    if(previousPiont){ 
-       context.strokeStyle='green'
-       context.beginPath()
-       context.moveTo(previousPiont.pageX,previousPiont.pageY)
-       context.lineTo(pageX,pageY)
-       context.stroke()    
-    }
-    previousPiont={'pageX':pageX,'pageY':pageY}
-  }else if(pen==='eraser'){
+    var pageX=e.touches[0].pageX
+    var pageY=e.touches[0].pageY
+
+    if(pen==='pen1'){
+        
+        if(previousPiont){ 
+           context.strokeStyle='green'
+           context.lineWidth=8
+           context.beginPath()
+           context.moveTo(previousPiont.pageX,previousPiont.pageY)
+           context.lineTo(pageX,pageY)
+           context.stroke()    
+        }
+        previousPiont={'pageX':pageX,'pageY':pageY}
+    }else if(pen==='pen2'){
+        if(previousPiont){ 
+            context.strokeStyle='green'
+            context.lineWidth=15
+            context.beginPath()
+            context.moveTo(previousPiont.pageX,previousPiont.pageY)
+            context.lineTo(pageX,pageY)
+            context.stroke()    
+        }
+      previousPiont={'pageX':pageX,'pageY':pageY}
+    }else if(pen==='eraser'){
     
-    context.clearRect(pageX-5, pageY-5, 10, 10)
-  }
+        context.clearRect(pageX-15, pageY-15, 30, 30)
+    }
    
 })
 
 canvas.addEventListener('touchend', function(){
-  previousPiont = null
+    previousPiont = null
 })
 
 save.onclick = function(){
-  var canvas=document.getElementById("canvas");
-  var data=canvas.toDataURL("image/png");
-  var newWindow=window.open('about:blank','image from canvas');
-  newWindow.document.write("<img src='"+data+"' alt='from canvas'/>");
+    var canvas=document.getElementById("canvas");
+    var data=canvas.toDataURL("image/png");
+    var newWindow=window.open('about:blank','image from canvas');
+    newWindow.document.write("<img src='"+data+"' alt='from canvas'/>");
 }
 
 clearAll.onclick=function(){
-  context.clearRect(0,30,canvas.width,canvas.height)
+    context.clearRect(0,0,canvas.width,canvas.height)
 }
